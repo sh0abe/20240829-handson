0a685968e3791a5555f274114fd07b05d337d705
Sho Abe <54127950+sh0abe@users.noreply.github.com>
2024-08-29 14:26:54 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Handson_ShoAbe"
+    Name = "Handson_ShoAbe_sample"
     TED = "20240829_Handson"
   }
 }
